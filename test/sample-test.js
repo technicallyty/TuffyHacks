ccecfbcdae59c2394f4de94ef015cdf9bc3663f9
technicallyty <48813565+tytech3@users.noreply.github.com>
2021-03-26 23:25:34 -0700

    token

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,14 +1,21 @@
+const { BigNumber } = require("@ethersproject/bignumber");
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-describe("Greeter", function() {
-  it("Should return the new greeting once it's changed", async function() {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    
-    await greeter.deployed();
-    expect(await greeter.greet()).to.equal("Hello, world!");
+describe("Token", function() {
+  it("Should create a token", async function() {
+    const Token = await ethers.getContractFactory("TuffyToken");
+    const token = await Token.deploy(2000000);
+    await token.deployed();
 
-    await greeter.setGreeting("Hola, mundo!");
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    const signers = await ethers.getSigners();
+    let addr1 = signers[1].getAddress();
+    await token._transfer(addr1, 35);
+    let tokens = await token._balanceOf(addr1);
+    console.log(tokens.toNumber());
+    //  expect(await token.greet()).to.equal("Hello, world!");
+
+    //  await greeter.setGreeting("Hola, mundo!");
+    //  expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
 });
